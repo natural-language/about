32078993662b748cd9b55b664199692950234409
alxfed <alxfed@gmail.com>
2019-01-25 10:11:10 -0600

    further

--- terminology.py
+++ terminology.py
@@ -7,6 +7,6 @@ Terminology in the form of lists
 # the main variable for symbolic 'equations'
 symbol = ['sign', 'emblem', 'token', 'mark', 'symptom']
 
-# the expression in letters of the symbol (that is
+# the expression in letters of a symbol (which is
 # representing a meaning).
 word = ['lemma', 'heading of annotation', 'heading of dictionary entry']
